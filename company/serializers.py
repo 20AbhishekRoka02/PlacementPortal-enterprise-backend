7cbe8c15d446116fcc814cf56425da441865ee6f
20AbhishekRoka02 <75659036+20AbhishekRoka02@users.noreply.github.com>
2026-05-03 15:32:37 +0530

    Create and Update Company API

--- company/serializers.py
+++ company/serializers.py
@@ -1,7 +1,45 @@
 from rest_framework.serializers import ModelSerializer
+from users.serializers import UpdateUserSerializer
 from .models import Company
 
 class CompanySerializer(ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
+
+
+class CreateCompanySerializer(ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['user', 'name', 'website', 'hr_phone_number', 'hr_email']
+        extra_kwargs = {
+            'user': {'required': True},
+            'name': {'required': True},
+            'website': {'required': True},
+            'location': {'required': True},
+            'hr_phone_number': {'required': True},
+            'hr_email': {'required': True},
+        }
+
+class UpdateCompanySerializer(ModelSerializer):
+    user = UpdateUserSerializer()
+
+    class Meta:
+        model = Company
+        fields = ['user', 'name', 'website', 'hr_phone_number', 'hr_email']
+    
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', {})
+
+        # Update User fields
+        user = instance.user
+        for attr, value in user_data.items():
+            setattr(user, attr, value)
+        user.save()
+
+        # Update Company fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
--- company/views.py
+++ company/views.py
@@ -1,15 +1,42 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Company
-from .serializers import CompanySerializer
+from .serializers import CompanySerializer, UpdateCompanySerializer
 from rest_framework.response import Response
 
 # Create your views here.
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
-    serializer_class = CompanySerializer
+
+    def get_serializer_class(self):
+        serializer_classes = {
+            'list': CompanySerializer,
+            'retrieve': CompanySerializer,
+            'update': UpdateCompanySerializer,
+            'partial_update': UpdateCompanySerializer,
+            'create': CompanySerializer
+            }
+        print("Serializer: ", self.action)
+        return serializer_classes.get(self.action, CompanySerializer)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=201)
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response({'data': serializer.data})
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        # Return the updated data using the READ serializer
+        return Response(UpdateCompanySerializer(instance).data)
