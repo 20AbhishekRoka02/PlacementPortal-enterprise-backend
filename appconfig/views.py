77a35474205895e81fae795bbb561e5a35d8388e
20AbhishekRoka02 <75659036+20AbhishekRoka02@users.noreply.github.com>
2026-05-05 01:07:58 +0530

    Add Resume to admin panel and fix side menu items as per Company and Student

--- appconfig/views.py
+++ appconfig/views.py
@@ -3,18 +3,31 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from users.models import User
+from users.models import User, UserRole
 from appconfig.models import SideNavItem
 from appconfig.serializers import SideNavItemSerializer
 # Create your views here.
 @api_view(['GET'])
 def get_sidenav_items(request):
-    print("request.user: ", request.user)
-    sidemenuitems = list()
+    sidemenuitems = [
+        {
+            "name": "Job Postings",
+            "screen_name": "job_postings",
+        },
+        {
+            "name": "My Applications",
+            "screen_name": "my_applications",
+        },
+    ]
     if isinstance(request.user, User) and request.user.is_authenticated:
-        sidenavitems = SideNavItem.objects.all()
-        sidenavitems_data = SideNavItemSerializer(sidenavitems, many=True).data
-        return Response({"sidenavitems": sidenavitems_data}, status=status.HTTP_200_OK)
+
+        if request.user.role == UserRole.STUDENT:
+            sidemenuitems.append({
+                "name": "Resume Generator",
+                "screen_name": "Resume Generator",
+            })
+
+        return Response({"sidenavitems": sidemenuitems}, status=status.HTTP_200_OK)
     else:
         return Response({"error": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED)
         
